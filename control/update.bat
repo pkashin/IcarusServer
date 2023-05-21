fc1ef07c6e9b8e7c45e58a57b11e6ea5393b6bbe
pkashin <48813179+pkashin@users.noreply.github.com>
2023-05-21 21:09:51 +0700

    test3

--- control/update.bat
+++ control/update.bat
@@ -1,3 +1,5 @@
 taskkill /pid IcarusServer-Win64-Shipping.exe
 cd "C:\steamcmd"
 steamcmd +login anonymous +force_install_dir "C:\Icarus" +app_update 2089300 validate +quit
+cd "C:\Icarus\Control"
+start.bat
--- start.bat
+++
@@ -1,2 +0,0 @@
-taskkill /pid IcarusServer-Win64-Shipping.exe
-start C:\Icarus\IcarusServer.exe -SteamServerName="SERVER_NAME" -Port=1777 -QueryPort=27015 -Log
