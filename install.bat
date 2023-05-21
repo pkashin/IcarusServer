865f06d0b59e6c32302907a9774318464a00731e
pkashin <48813179+pkashin@users.noreply.github.com>
2023-05-21 12:04:52 +0700

    the test.bat file has been deleted. Content moved to install.bat

--- install.bat
+++ install.bat
@@ -4,8 +4,12 @@ netsh advfirewall firewall add rule name="Icarus open PORT 27015/udp" protocol=U
 
 :: additional software for Windows Server 2022
 curl -o VC_redist.x64.exe https://download.visualstudio.microsoft.com/download/pr/eaab1f82-787d-4fd7-8c73-f782341a0c63/917C37D816488545B70AFFD77D6E486E4DD27E2ECE63F6BBAAF486B178B2B888/VC_redist.x64.exe
+VC_redist.x64.exe /install /quiet /norestart
+del VC_redist.x64.exe
 curl -o directx_Jun2010_redist.exe https://download.microsoft.com/download/8/4/A/84A35BF1-DAFE-4AE8-82AF-AD2AE20B6B14/directx_Jun2010_redist.exe
 mkdir "C:\dx"
+directx_Jun2010_redist.exe /Q /T:"C:\dx"
+del directx_Jun2010_redist.exe
 
 :: download and install steamcmd
 curl -o steamcmd.zip https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip
@@ -13,4 +17,12 @@ mkdir "C:\steamcmd"
 tar -xf steamcmd.zip -C "C:\steamcmd"
 del steamcmd.zip
 cd "C:\steamcmd"
-steamcmd +login anonymous +force_install_dir "C:\Icarus" +app_update 2089300 validate +quit
+steamcmd +login anonymous +force_install_dir "C:\icarus" +app_update 2089300 validate +quit
+
+:: install directx
+cd "C:\dx"
+DXSETUP.exe /silent
+
+:: delete the folder with the directx installer
+cd ..
+rmdir /s /q "C:\dx"
--- test.bat
+++
@@ -1,4 +0,0 @@
-VC_redist.x64.exe /install /quiet /norestart
-directx_Jun2010_redist.exe /Q /T:"C:\dx"
-cd "C:\dx"
-DXSETUP.exe /silent
